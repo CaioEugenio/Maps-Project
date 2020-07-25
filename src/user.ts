76bb50044de2a3d3c100063300f72f00966058ce
Caio <eugeniocaio10@gmail.com>
2020-07-25 00:08:28 -0400

    Created constructor for user

--- src/user.ts
+++ src/user.ts
@@ -7,5 +7,11 @@ class User {
     lng: number;
   };
 
-  constructor(name: string, locatr);
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
