f8e1a4276a18669ff11c5affd75c3753b014b44b
Caio <eugeniocaio10@gmail.com>
2020-08-03 19:53:58 -0400

    Changed to interface

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,13 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -5,6 +15,16 @@ export class CustomMap {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
     });
   }
 }
